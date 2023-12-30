38b7c3fc7c84e7596dd20baa2945fab0ccdb9a3f
d33znu75 <61992377+RedaHmimchi@users.noreply.github.com>
2023-12-30 16:29:54 +0100

    Add files via upload

--- push_swap.c
+++ push_swap.c
@@ -6,23 +6,52 @@
 /*   By: rhmimchi <rhmimchi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 16:48:11 by rhmimchi          #+#    #+#             */
-/*   Updated: 2023/12/27 22:53:45 by rhmimchi         ###   ########.fr       */
+/*   Updated: 2023/12/30 16:26:19 by rhmimchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./src/push_swap.h"
 #include <stdio.h>
 
-void	print_stack(t_stack s)
+// void	print_stack(t_stack s)
+// {
+// 	int	j;
+
+// 	j = 0;
+// 	while (j < s.len)
+// 	{
+// 		printf("stack_a->list[%d] = %ld\n", j, s.list[j]);
+// 		j++;
+// 	}
+// }
+
+void	chunks_1(t_stack a, t_stack b)
 {
+	int	i;
 	int	j;
 
-	j = 0;
-	while (j < s.len)
+	i = 0;
+	j = a.len;
+	_indexing_numbers(&a);
+	while (i <= j)
 	{
-		printf("stack_a->list[%d] = %ld\n", j, s.list[j]);
-		j++;
+		while (find_min(&a) < i && a.len)
+		{
+			if (a.list[0] >= 0 && a.list[0] < i)
+				pb(&a, &b);
+			else if (a.list[0] >= i + j / 1 && a.list[0] < i + j * 2 / 1)
+			{
+				pb(&a, &b);
+				rb(&b);
+			}
+			else
+				ra(&a);
+		}
+		i += j / 1;
 	}
+	while (a.len)
+		pb(&a, &b);
+	b_sort(a, b);
 }
 
 int	main(int argc, char *argv[])
@@ -44,9 +73,9 @@ int	main(int argc, char *argv[])
 		sa(&a);
 	else if (a.len == 3)
 		three_sort(a);
-	else if (a.len > 3 && a.len <= 10)
-		_sort(a, b);
-	else if (a.len > 10 && a.len < 300)
+	else if (a.len > 3 && a.len <= 7)
+		chunks_1(a, b);
+	else if (a.len > 7 && a.len < 300)
 		chunks_5(a, b);
 	else
 		chunks_11(a, b);
